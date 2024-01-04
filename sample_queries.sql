5fc1dbc1d5db7adca493697127bd348322544ebf
Stephen <skent89@gmail.com>
2024-01-04 18:58:08 -0500

    changed sample query

--- sample_queries.sql
+++ sample_queries.sql
@@ -6,22 +6,28 @@
 WITH FIND_FRED AS (
     SELECT
         tb.tconst AS title_id,
-        tb.primarytitle as title
+        tb.primarytitle as title,
+        tb.startyear as year
     FROM name_basics nb
     INNER JOIN title_principals tp ON nb.nconst = tp.nconst
     INNER JOIN title_basics tb ON tp.tconst = tb.tconst
-    WHERE nb.primaryname = 'Fred Astaire'
+    WHERE
+        nb.primaryname = 'Fred Astaire'
+        AND tb.startyear < '1990' -- three years after he died
 )
 
 SELECT
     ff.title_id,
+    ff.year,
     ff.title,
     nb.primaryname AS director
 FROM FIND_FRED ff
 INNER JOIN title_directors td ON ff.title_id = td.tconst
-INNER JOIN name_basics nb ON td.director_nconst = nb.nconst;
+INNER JOIN name_basics nb ON td.director_nconst = nb.nconst
+ORDER BY ff.year;
 
--- this should yield something like this:
+
+-- this should yield something likeC; this:
 
 -- tt0025164,The Gay Divorcee,Mark Sandrich
 -- tt0026942,Roberta,William A. Seiter
